abed3f94c1476e319b1e89faf85efa44f6738d11
xmuammar@gmail.com <Mynameisasus>
2022-10-12 15:14:46 +0700

    perbaharui sintak peta

--- lihat_peta.php
+++ lihat_peta.php
@@ -6,7 +6,13 @@
         $longitude = $_POST["longitude"];
         ?>
   
-		<iframe  width="100%" height="100%" src="https://maps.google.com/maps?q=<?php echo $latitude; ?>,<?php echo $longitude; ?>&output=embed"></iframe>
+		<!-- <iframe  width="100%" height="100%" src="https://maps.google.com/maps?q=<?php echo $latitude; ?>,<?php echo $longitude; ?>&output=embed"></iframe> -->
+            
+        <iframe width="100%" height="100%" src="https://maps.google.com/maps?q=<?php echo $latitude; ?>,<?php echo $longitude; ?>&t=k&z=19&ie=UTF8&iwloc=&output=embed"></iframe>
+          
+
+            
+
  
         <?php
     }
